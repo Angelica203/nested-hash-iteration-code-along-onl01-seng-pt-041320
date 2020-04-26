152e2a99071a4a18f1738039dc3ab1a1d94fe91e
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-26 22:46:34 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,8 +15,12 @@ require 'pry'
   }
 
 def remove_strawberry(contacts)
-  contacts.each do |
-
-  # remove_strawberry[:contacts] = "Freddy Mercury"[favorite_ice_cream_flavors]
-
-end
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          binding.pry
+        end
+      end
+    end
+  end
